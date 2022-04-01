a59d40f10f301b0dbedd8fb2b9bb26b4a501aa52
TP00214906 <gulati.parth98@gmail.com>
2022-04-01 05:34:46 +0530

    Yez, completed in one go. Single commit. Bad for business.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import {GiTriangleTarget} from 'react-icons/gi'
+import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+
+  useEffect(()=>{
+    gsap.to('.visual-1', {
+      width: 0,
+      scrollTrigger: {
+        trigger: '.App',
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        scrub: 1,
+        pin: true,
+        anticipatePin: 1
+      }
+    })
+
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="visual-1"></div>  
+      <div className="visual-2"></div>
+        <h1 className="heading-1">Pattypan</h1>
+        <GiTriangleTarget size={100} id="triangle-1"/>
+        <GiTriangleTarget size={80} id="triangle-2"/>
+        <GiTriangleTarget size={90} id="triangle-3"/>
+        <GiTriangleTarget size={110} id="triangle-4"/>
     </div>
   );
 }
